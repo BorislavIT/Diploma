2e799ce2589e94f0310606fa679f5d360cbecb6f
Borislav Gegovski <borislavgegovski@gmail.com>
2024-07-01 16:42:03 +0300

    added missing function due to merges

--- Client/src/shared/utilities.ts
+++ Client/src/shared/utilities.ts
@@ -5,3 +5,11 @@ export const combineMp3PathWithFile = (file: string) => {
 export const combineThumbnailPathWithFile = (file: string) => {
   return `${process.env.NEXT_PUBLIC_REACT_APP_BASE_API_URL}/thumbnails/${file}`;
 };
+
+export function encodeUrl(url?: string) {
+  const parts = url?.split("/");
+  const fileName = parts?.pop()!;
+  const encodedFileName = encodeURIComponent(fileName);
+  const encodedUrl = [...(parts ?? []), encodedFileName].join("/");
+  return encodedUrl;
+}
